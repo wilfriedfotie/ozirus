15e466299c9e1fbfadb19f3b5a1ae19b2164d0ab
Wilfried Fotie <contact.fotie@gmail.com>
2026-06-02 11:16:41 +0100

    feat: add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { DM_Sans } from 'next/font/google';
+import { Analytics } from "@vercel/analytics/next"
 import GlobalLoader from '@/components/GlobalLoader';
 import SharedNav from '@/components/SharedNav';
 import './globals.css';
